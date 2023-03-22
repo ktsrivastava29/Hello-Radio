53bea36e72dff09011fef4aa3722c20401ad2d8c
ktsrivastava29 <ktsrivastava29@gmail.com>
2023-03-22 12:00:14 +0530

    initial

---
+++ lib/utils/ai_util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static Color primaryColor1 = Color.fromARGB(255, 43, 41, 40);
+  static Color primaryColor2 = Color.fromARGB(255, 71, 163, 120);
+}
